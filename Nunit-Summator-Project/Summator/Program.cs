235851f94562b809ba677d437e291475c28fa901
milev91 <105004729+milev91@users.noreply.github.com>
2022-05-09 13:34:31 +0300

    add test

---
+++ Nunit-Summator-Project/Summator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Summator
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+            if (sum==21)
+            {
+                Console.WriteLine("Test PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FEIL");
+            }
+
+            double average = Summator.Average(new int[] { 5, 10, 15 });
+            if (average == 10)
+            {
+                Console.WriteLine("Test PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FEIL");
+            }
+
+        }
+    }
+}
